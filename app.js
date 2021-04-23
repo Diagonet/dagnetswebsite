739cb2b2b8949cfaf8b6b66966bd314f17f95754
Erik Tabs <erik.teruaki@gmail.com>
2021-04-22 23:49:18 -0300

    added a text box used by the randomGreeting function

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+'use strict';
 function randomGreeting() {
     const greeting = ['Gud\'ay', 'Goede dag', 'Bom dia', 'Hello there'];
-    let chosenGreeting = greeting[Math.floor(Math.random() * greeting.length)];
+    let name = document.getElementById("name").value;
+    if (!name) {
+        name='nobody';
+    }
+    let chosenGreeting = greeting[Math.floor(Math.random() * greeting.length)]+', '+name+'!';
     alert ('Here is you free* random greeting:\n\n'+chosenGreeting);
 
 }
