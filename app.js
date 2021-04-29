0de37ed9923a9e4e7098747ef8a41583a6f3e073
Erik Tabs <erik.teruaki@gmail.com>
2021-04-28 22:31:22 -0300

    added the first 2 jquery events and functions

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ function randomGreeting() {
     greeting[Math.floor(Math.random() * greeting.length)] + ", " + name + "!";
   alert("Here is you free* random greeting:\n\n" + chosenGreeting);
 }
+$(document).ready(function () {
+  $("nav a").hover(
+    function () {
+      $(this).css("font-weight", "900");
+    },
+    function () {
+      $(this).css("font-weight", "normal");
+    }
+  );
+  $("img").click(function () {
+    if ($(this).css("width") == "200px") {
+      $(this).css("width", "100%");
+    } else {
+      $(this).css("width", "200px");
+    }
+  });
+});
