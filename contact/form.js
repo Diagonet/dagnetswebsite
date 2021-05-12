45e43dac2c83f496f72bdf334c3596ed8a652920
Erik Tabs <erik.teruaki@gmail.com>
2021-05-11 23:46:30 -0300

    finalized the javascript and form html

--- contact/form.js
+++ contact/form.js
@@ -1,13 +1,17 @@
-function SubForm() {
-  $.ajax({
-    url: "https://api.apispreadsheets.com/data/12250/",
-    type: "post",
-    data: $("#myForm").serializeArray(),
-    success: function () {
-      alert("Form Data Submitted :)");
-    },
-    error: function () {
-      alert("There was an error :(");
-    },
+function sendingMsgs() {
+  $("#messageForm").submit(function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "https://api.apispreadsheets.com/data/12250/",
+      type: "post",
+      data: $("#messageForm").serializeArray(),
+      success: function () {
+        alert("You message has been sent!");
+      },
+      error: function () {
+        alert("There was an error");
+      },
+    });
   });
 }
+$(sendingMsgs);
